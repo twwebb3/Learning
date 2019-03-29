20b532d2ea85ca56cb682727ead1734b62d27318
T W Webb <TW.Webb@ilg.com>
2019-03-29 15:07:11 -0400

    Signed-off-by: T W Webb

--- PyTorch/mnistClassifierPyTorch.py
+++ PyTorch/mnistClassifierPyTorch.py
@@ -119,4 +119,24 @@ for epoch in range(2):
 print('Finished training!')
 
 
+# testing
+detaiter = iter(testloader)
+images, labels = detaiter.next()
 
+classes = [0,1,2,3,4,5,6,7,8,9]
+# print images
+plt.imshow(torchvision.utils.make_grid(images[1]))
+print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(10)))
+
+output = net(images)
+
+_, predicted = torch.max(output, 1)
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(10)))
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        
