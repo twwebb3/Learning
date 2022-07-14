1ca92dc67d32ba2ec2862e5432be8890e7cdb964
TW Webb <twwebb1@me.com>
2022-07-13 23:55:10 -0400

    Initial base testing C++ terminal app.

---
+++ LearnCpp/baseTest.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	double numberX, numberY, output;
+
+	cout << "Enter first number: ";
+	cin >> numberX;
+
+	cout << "Enter second number: ";
+	cin >> numberY;
+
+	output = numberX*numberY;
+
+	cout << "The answer is " << output;
+	return 0;
+}
