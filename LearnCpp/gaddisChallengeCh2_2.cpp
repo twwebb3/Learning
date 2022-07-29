31bbd4ca69882793b215373e0ead05cf51fb5760
TW Webb <twwebb1@me.com>
2022-07-28 21:39:09 -0400

    Chapter 2 challenge 2 complete successfully.

--- LearnCpp/gaddisChallengeCh2_2.cpp
+++ LearnCpp/gaddisChallengeCh2_2.cpp
@@ -4,3 +4,21 @@ of total sales. Based on that percentage, write a program that
 will predict how much the East Coast division will generate if
 the company has $4.6 million in sales this year
 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sales=4600000, total;
+	float eastCoastShare=0.62;
+
+	total=sales*eastCoastShare;
+
+	cout << "Total East Coast sales: "
+		 << total
+		 << endl;
+
+	return 0;
+
+}
