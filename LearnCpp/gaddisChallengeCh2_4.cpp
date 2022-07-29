c12139b88895d384dcdc1252fd02aa6849776aa6
TW Webb <twwebb1@me.com>
2022-07-28 22:30:58 -0400

    Chapter 2 challenge 4 completed successfully.

--- LearnCpp/gaddisChallengeCh2_4.cpp
+++ LearnCpp/gaddisChallengeCh2_4.cpp
@@ -5,3 +5,32 @@ bill for a patron with a $44.50 meal charge. The tax should be
 of the total after adding the tax. Display the meal coast, tax
 amount, tip amount and total bill on the screen.
 */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	float mealCost=44.50,
+		  taxRate=0.0675,
+		  tipRate=0.15,
+		  taxAmount,
+		  mealTotal,
+		  tipAmount,
+		  totalBill;
+
+
+	taxAmount=ceilf(mealCost*taxRate*100)/100;
+	mealTotal=mealCost+taxAmount;
+	tipAmount=ceilf(mealTotal*tipRate*100)/100;
+	totalBill=mealTotal+tipAmount;
+
+	cout << "Meal Cost: " << mealCost << endl
+		 << "Tax Amount: " << taxAmount << endl
+		 << "Tip Amount: " << tipAmount << endl
+		 << "Total Bill: " << totalBill << endl;
+
+
+	return 0;
+}
