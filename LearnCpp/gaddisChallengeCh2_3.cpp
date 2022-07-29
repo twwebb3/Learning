83f63ea8da24da1ec550b945043148ea717c2770
TW Webb <twwebb1@me.com>
2022-07-28 21:47:33 -0400

    Chapter 2 challenge 3 completed successfully.

--- LearnCpp/gaddisChallengeCh2_3.cpp
+++ LearnCpp/gaddisChallengeCh2_3.cpp
@@ -3,3 +3,24 @@ Write a program that will compute the total sales tax on a
 $52 purchase. Assume the state sales tax is 4 percent and 
 the county sales tax is 2 percent.
 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int purchaseAmount=52;
+	float stateSalesTaxRate=0.04, 
+		  countySalesTaxRate=0.02, 
+		  stateSalesTax,
+		  countySalesTax,
+		  totalSalesTax;
+
+	stateSalesTax=purchaseAmount*stateSalesTaxRate;
+	countySalesTax=purchaseAmount*countySalesTaxRate;
+	totalSalesTax=stateSalesTax+countySalesTax;
+
+	cout << "The total tax is: " << totalSalesTax << endl;
+
+	return 0;
+}
