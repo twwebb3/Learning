6631bfd1db3e3ff737dcc28e3633d22a5850a914
TW Webb <twwebb1@me.com>
2022-07-31 19:26:40 -0400

    Gaddis Chapter 2 challenge 9 completed successfully.

--- LearnCpp/gaddisChallengeCh2_9.cpp
+++ LearnCpp/gaddisChallengeCh2_9.cpp
@@ -8,3 +8,16 @@ C++ program that will determine the amount of memory
 used by these types and display the information on the
 screen.
 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Data type char uses " << sizeof(char) << " bytes." << endl
+		 << "Data type int uses " << sizeof(int) << " bytes." << endl
+		 << "Data type float uses " << sizeof(float) << " bytes." << endl
+		 << "Data type double uses " << sizeof(double) << " bytes." << endl;
+    
+    return 0;
+}
