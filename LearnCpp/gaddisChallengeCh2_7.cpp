5dc9496fbe322fc08c400a331626e56d8324b5be
TW Webb <twwebb1@me.com>
2022-07-31 19:03:18 -0400

    Gaddis Chapter 2 challenge 7 completed successfully.

--- LearnCpp/gaddisChallengeCh2_7.cpp
+++ LearnCpp/gaddisChallengeCh2_7.cpp
@@ -9,3 +9,24 @@ per year, write a program that displays:
 - The number of mm higher than the current level that the
   ocean's level will be in 10 years
 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double riseLevel = 1.5,
+		   fiveYearLevel,
+		   sevenYearLevel,
+		   tenYearLevel;
+
+	fiveYearLevel=riseLevel*5;
+	sevenYearLevel=riseLevel*7;
+	tenYearLevel=riseLevel*10;
+
+	cout << "The ocean level will be " << fiveYearLevel << "mm higher in 5 years." << endl <<
+			"The ocean level will be " << sevenYearLevel << "mm higher in 7 years." << endl <<
+			"The ocean level will be " << tenYearLevel << "mm higher in 10 years." << endl;
+
+	return 0;
+}
