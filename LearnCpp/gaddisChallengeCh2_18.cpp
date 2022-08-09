3529c5fac362b2ea7e9f2b91a38a0ca76393c29d
TW Webb <twwebb1@me.com>
2022-08-08 22:24:05 -0400

    Gaddis Chapter 2 challenge 18 completed successfully.

--- LearnCpp/gaddisChallengeCh2_18.cpp
+++ LearnCpp/gaddisChallengeCh2_18.cpp
@@ -11,4 +11,24 @@ displays the following:
 * The approximate number of customers in the survey who
   perfer citrus flavored energy drinks.
   */
-  
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int surveyedCustomers=12476;
+	double multiplePurchaseRate=0.14,
+		   citrusEnjoyerRate=0.64,
+		   multiplePurchasers,
+		   citrusEnjoyers;
+
+	multiplePurchasers=ceil(surveyedCustomers*multiplePurchaseRate);
+	citrusEnjoyers=ceil(surveyedCustomers*citrusEnjoyerRate);
+
+	cout << "Number of customers that purchase one or more energy drinks per week: " << multiplePurchasers << endl
+		 << "Number of customers that prefere citrus flavored energy drinks: " << citrusEnjoyers << endl;
+
+	return 0;
+}
