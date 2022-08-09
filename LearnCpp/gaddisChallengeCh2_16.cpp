f3684f26b447279f7805e0d07e908be73275d896
TW Webb <twwebb1@me.com>
2022-08-08 22:05:43 -0400

    Gaddis Chapter 2 challenge 16 completed successfully.

--- LearnCpp/gaddisChallengeCh2_16.cpp
+++ LearnCpp/gaddisChallengeCh2_16.cpp
@@ -9,3 +9,20 @@ Write a program that displays the following pattern:
   ***
    *
 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	cout << "   *" << endl
+		 << "  ***" << endl
+		 << " *****" << endl
+		 << "*******" << endl
+		 << " *****" << endl
+		 << "  ***" << endl
+		 << "   *" << endl;
+
+	return 0;
+}
