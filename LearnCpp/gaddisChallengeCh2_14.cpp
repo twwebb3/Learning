8657fd5a37101a0c5280ade86b5ed5cf4f813d8d
TW Webb <twwebb1@me.com>
2022-08-08 21:59:34 -0400

    Gaddis Chapter 2 challenge 14 successfully completed.

--- LearnCpp/gaddisChallengeCh2_14.cpp
+++ LearnCpp/gaddisChallengeCh2_14.cpp
@@ -5,7 +5,20 @@ each on a separate line:
 Your name
 Your address, with city, state, and ZIP code
 Your telephone number
-Your college majore
+Your college major
 
 Use only a single cout statement to display all of this information.
 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "TW Webb" << endl
+		 << "123 Main St., Boston, MA 02108" << endl
+		 << "+1 (850) 867-5309" << endl
+		 << "Economics" << endl;
+
+	return 0;
+}
