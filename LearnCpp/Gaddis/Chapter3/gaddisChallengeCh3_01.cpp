b47591e1d25e6578e728c9bb449d96c19711ecb5
TW Webb <twwebb1@me.com>
2022-09-07 21:40:24 -0400

    Chapter 3 challenge 1 successfully completed.

--- LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_01.cpp
+++ LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_01.cpp
@@ -6,3 +6,22 @@ miles it can be driven on a full tank. It should then
 display the number of miles that may be driven per
 gallon of gas.
 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float tankCapacity, tankRange, mpg;
+
+	cout << "How many gallons of gas can the car hold? ";
+	cin >> tankCapacity;
+	cout << "How many miles can the car be driven on a full tank? ";
+	cin >> tankRange;
+
+	mpg = tankRange/tankCapacity;
+
+	cout << "The car gets " << mpg << "MPG." << endl;
+
+	return 0;
+}
