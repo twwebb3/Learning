0cbd842fc7cab48b223fcf8d9e100c1e6bab52f6
TW Webb <twwebb1@me.com>
2022-09-07 21:48:13 -0400

    Chapter 3 challenge 2 mostly complete, sort decimal issue.

--- LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_02.cpp
+++ LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_02.cpp
@@ -8,3 +8,34 @@ sales. Format your dollar amount in fixed-point notations,
 with two decimal places of precision, and be sure the
 decimal point is always dipslayed.
 */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int aPrice=15, 
+		aSold,
+		bPrice=12,
+		bSold,
+		cPrice=9,
+		cSold,
+		income;
+
+	cout << "How many Clas A tickets were sold? ";
+	cin >> aSold;	
+	cout << "How many Clas B tickets were sold? ";
+	cin >> bSold;
+	cout << "How many Clas C tickets were sold? ";
+	cin >> cSold;
+
+	income = aSold*aPrice + bSold*bPrice + cSold*cPrice;
+
+	income = ceil(income*100)/100;   // need to make sure decimals are always showing 2, does not currently
+
+	cout << "Total Income: " << income << endl;
+
+	return 0;
+}
