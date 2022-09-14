301b2641765ea56612cabd58d70d9c67dce69a7c
TW Webb <twwebb1@me.com>
2022-09-14 11:43:13 -0400

    Complete.

--- LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_07.cpp
+++ LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_07.cpp
@@ -6,3 +6,29 @@ that asks the user to input how many cookies he or she
 actually ate and then reports how many total calories were
 consumed.
 */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int cookiesInBag=40,
+		servings=10,
+		cookiesPerServing,
+		caloriesPerServing=300;
+
+	float caloriesConsumed,
+		  cookiesAte;
+
+	cookiesPerServing=cookiesInBag/servings;
+
+	cout << "How many cookies did you eat? ";
+	cin >> cookiesAte;
+
+	caloriesConsumed = caloriesPerServing * (cookiesAte/cookiesPerServing);
+
+	cout << "You consumed " << caloriesConsumed << " calories." << endl;
+
+	return 0;
+}
