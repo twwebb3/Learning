f2adf790cbf29826d3dfa334a16c039e22be5368
TW Webb <twwebb1@me.com>
2022-09-14 11:25:42 -0400

    Chapter 3 challenge 5 mostly complete, formatting remains.

--- LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_05.cpp
+++ LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_05.cpp
@@ -15,3 +15,41 @@ Gross Box Office Profit:	$ 2673.00
 Net Box Office Profit:		$  534.60
 Amount Paid to Distributor:	$ 2138.40
 */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const int SIZE=20;
+	char movieName[SIZE];
+	int adultSold, childSold;
+	float adultPrice=6.00,
+		  childPrice=3.00,
+		  grossProfit,
+		  netProfit,
+		  distributorShare=.8,
+		  distributorAmount;
+
+	cout << "What is the name of the movie?";
+	cin.getline(movieName, SIZE);
+	cout << "How many adult tickets were sold?";
+	cin >> adultSold;
+	cout << "How many child tickets were sold?";
+	cin >> childSold;
+
+	grossProfit = adultPrice*adultSold + childPrice*childSold;
+	distributorAmount = grossProfit*distributorShare;
+	netProfit = grossProfit - distributorAmount;
+
+	cout << "Movie Name: " << movieName << endl
+		 << "Adult Tickets Sold: " << adultSold << endl
+		 << "Child Tickets Sold: " << childSold << endl
+		 << "Gross Box Office Profit: " << grossProfit << endl
+		 << "Net Box Office Profit: " << netProfit << endl
+		 << "Amount Paid to Distributor: " << distributorAmount << endl;
+
+	return 0;
+
+}
