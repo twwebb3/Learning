006cf69b1a3ac910e342aca6f7f04e2547a3b382
TW Webb <twwebb1@me.com>
2022-12-02 23:50:05 -0500

    Chapter 3 challenge 10 complete.

--- LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_10.cpp
+++ LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_10.cpp
@@ -5,3 +5,25 @@ F = (9/5)C + 32
 
 F is the Fahrenheit temperature and C is the Celsius temperature.
 */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    // Variables
+    double celsius, fahrenheit;
+    
+    // Get user input
+    cout << "Enter a Celsius temperature: ";
+    cin >> celsius;
+    
+    // Calculate Fahrenheit temperature
+    fahrenheit = (9.0/5.0) * celsius + 32;
+    
+    // Display Fahrenheit temperature
+    cout << "The Fahrenheit equivalent is " << fahrenheit << endl;
+    
+    return 0;
+}
