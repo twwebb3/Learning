361829ff22091a0a6bb3010a5d501356712eaff5
TW Webb <twwebb1@me.com>
2022-12-02 22:32:28 -0500

    Chapter 3 challenge 9 complete.

--- LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_09.cpp
+++ LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_09.cpp
@@ -7,3 +7,39 @@ maintenance. The program should then display the
 total monthly cost of these expenses, and the
 total annual cost of these expenses.
 */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    // Variables
+    double loanPayment, insurance, gas, oil, tires, maintenance, totalMonthly, totalAnnual;
+    
+    // Get user input
+    cout << "Enter the monthly cost for the following expenses incurred from operating your automobile: " << endl;
+    cout << "Loan payment: ";
+    cin >> loanPayment;
+    cout << "Insurance: ";
+    cin >> insurance;
+    cout << "Gas: ";
+    cin >> gas;
+    cout << "Oil: ";
+    cin >> oil;
+    cout << "Tires: ";
+    cin >> tires;
+    cout << "Maintenance: ";
+    cin >> maintenance;
+    
+    // Calculate total monthly and annual costs
+    totalMonthly = loanPayment + insurance + gas + oil + tires + maintenance;
+    totalAnnual = totalMonthly * 12;
+
+    // Display total monthly and annual costs
+    cout << "The total monthly cost of these expenses is $" << totalMonthly << endl;
+    cout << "The total annual cost of these expenses is $" << totalAnnual << endl;
+
+    return 0;
+}
+
