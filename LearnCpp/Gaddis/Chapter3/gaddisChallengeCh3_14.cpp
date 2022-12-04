80bfc6b24fc77cdda48c703ebaeb61ca22208ae1
TW Webb <twwebb1@me.com>
2022-12-04 00:15:55 -0500

    Chapter 3 challenge 14 complete.

--- LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_14.cpp
+++ LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_14.cpp
@@ -16,3 +16,38 @@ report how much annual property tax senior homeowner
 will be charged for this property and what the
 quarterly tax bill will be.
 */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    // Variables
+    double actualValue, currentTax, assessmentValue, propertyTax, seniorPropertyTax, seniorQuarterlyTax;
+    
+    // Get user input
+    cout << "Enter the actual value of a piece of property: ";
+    cin >> actualValue;
+
+    cout << "Enter the current tax rate for each $100 of assessed value: ";
+    cin >> currentTax;
+    currentTax = currentTax / 100;
+    
+    // Calculate assessment value
+    assessmentValue = actualValue * 0.6;
+    
+    // Calculate senior citizen property tax
+    seniorPropertyTax = (assessmentValue - 5000) * currentTax;
+    
+    // Calculate senior citizen quarterly tax
+    seniorQuarterlyTax = seniorPropertyTax / 4;
+    
+    // Display report
+    cout << "Assessment Value: $" << setw(10) << assessmentValue << endl;
+    cout << "Senior Citizen Property Tax: $" << setw(10) << seniorPropertyTax << endl;
+    cout << "Senior Citizen Quarterly Tax: $" << setw(10) << seniorQuarterlyTax << endl;
+    
+    return 0;
+}
+
