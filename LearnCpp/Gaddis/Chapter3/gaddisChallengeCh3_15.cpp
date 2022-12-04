57007e14ce6f232ec73c20ca246e111a0a3c84c9
TW Webb <twwebb1@me.com>
2022-12-04 00:44:32 -0500

    Chapter 3 challenge 15 complete.

--- LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_15.cpp
+++ LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_15.cpp
@@ -17,3 +17,35 @@ program will display the correct solution:
 _____
   376
 */
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    // Set the random number seed
+    srand(time(0));
+
+    // Generate two random numbers
+    int num1 = rand() % 1000;
+    int num2 = rand() % 1000;
+
+    // Display the problem
+    cout << num1 << endl;
+    cout << "+" << num2 << endl;
+    cout << "_____" << endl;
+
+    // Pause the program
+    cout << "Press any key to continue...";
+    cin.get();
+
+    // Display the solution
+    cout << num1 << endl;
+    cout << "+" << num2 << endl;
+    cout << "_____" << endl;
+    cout << num1 + num2 << endl;
+
+    return 0;
+}
