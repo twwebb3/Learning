e309658d03ad414d001f2155de866daeb197519b
TW Webb <twwebb1@me.com>
2022-12-03 20:02:29 -0500

    Chapter 3 challenge 13 complete.

--- LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_13.cpp
+++ LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_13.cpp
@@ -8,3 +8,30 @@ the acre assessed at ^6,000 will be $38.40. Write a
 program that asks for the actual value of a piece of
 property and displays the assessment value and property tax.
 */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    // Variables
+    double actualValue, assessmentValue, propertyTax;
+    
+    // Get user input
+    cout << "Enter the actual value of a piece of property: ";
+    cin >> actualValue;
+    
+    // Calculate assessment value
+    assessmentValue = actualValue * 0.6;
+    
+    // Calculate property tax
+    propertyTax = assessmentValue * 0.0064;
+    
+    // Display report
+    cout << "Assessment Value: $" << setw(10) << assessmentValue << endl;
+    cout << "Property Tax: $" << setw(16) << propertyTax << endl;
+    
+    return 0;
+}
+
