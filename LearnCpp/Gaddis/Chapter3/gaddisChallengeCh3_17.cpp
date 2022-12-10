ebbb66d040b487021f59edb9cbaa0b46a20e0fa1
TW Webb <twwebb1@me.com>
2022-12-09 22:45:54 -0500

    Chapter 3 challenge 17 complete.

--- LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_17.cpp
+++ LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_17.cpp
@@ -16,3 +16,44 @@ Amount Paid Back:			$ 11957.15
 Interest Paid:				$  1957.15
 
 */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    // Declare variables
+    double loan, rate, payments, payment, amount, interest;
+
+    // Get the loan amount
+    cout << "Enter the loan amount: ";
+    cin >> loan;
+
+    // Get the monthly interest rate
+    cout << "Enter the monthly interest rate: ";
+    cin >> rate;
+
+    // Get the number of payments
+    cout << "Enter the number of payments: ";
+    cin >> payments;
+
+    // Calculate the monthly payment
+    payment = loan * (rate * pow((1 + rate), payments)) / (pow((1 + rate), payments) - 1);
+
+    // Calculate the amount paid back
+    amount = payment * payments;
+
+    // Calculate the interest paid
+    interest = amount - loan;
+
+    // Display the report
+    cout << "Loan Amount: $" << loan << endl;
+    cout << "Monthly Interest Rate: " << rate * 100 << "%" << endl;
+    cout << "Number of Payments: " << payments << endl;
+    cout << "Monthly Payment: $" << payment << endl;
+    cout << "Amount Paid Back: $" << amount << endl;
+    cout << "Interest Paid: $" << interest << endl;
+
+    return 0;
+}
