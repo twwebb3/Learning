fa4bbd677847b0616ebc9c8fe1603523692e7fe7
TW Webb <twwebb1@me.com>
2022-12-10 03:38:34 -0500

    Chapter 3 challenge 23 complete.

--- LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_23.cpp
+++ LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_23.cpp
@@ -2,3 +2,50 @@
 Modify the program you wrote for Programming Challenge 4 (Average Rainfall) so it
 reads its input from a file instead of the keyboard.
 */
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    const int SIZE=15;
+    // Declare variables
+    float totalRainfall;
+    float averageRainfall;
+    float rainfall;
+    int numMonths;
+    char month[SIZE];
+
+    // Open the file
+    ifstream inputFile;
+    inputFile.open("rainfall.txt");
+
+    // Read the number of months
+    inputFile >> numMonths;
+
+    // Read the rainfall for each month
+    for (int i = 0; i < numMonths; i++)
+    {
+        // Read the month
+        inputFile >> month;
+
+        // Read the rainfall
+        inputFile >> rainfall;
+
+        // Add the rainfall to the total
+        totalRainfall += rainfall;
+    }
+    //totalRainfall=12;
+
+    // Close the file
+    inputFile.close();
+
+    // Calculate the average rainfall
+    averageRainfall = totalRainfall / numMonths;
+
+    // Display the average rainfall
+    cout << "The average rainfall for " << numMonths << " months is " << averageRainfall << endl;
+
+    return 0;
+}
