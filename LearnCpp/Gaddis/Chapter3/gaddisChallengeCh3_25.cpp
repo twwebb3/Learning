c0433adf210c2bb97328e61333cce487c0afa86b
TW Webb <twwebb1@me.com>
2022-12-10 04:04:08 -0500

    Chapter 3 challenge 25 complete.

--- LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_25.cpp
+++ LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_25.cpp
@@ -18,3 +18,56 @@ AGE, NAME went to college at COLLEGE. NAME graduated and went to work
 as a PROFESSION. Then, NAME adopted a(n) ANIMAL named PETNAME. They
 both lived happily ever after!
 */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    const int SIZE=20;
+    // Declare variables
+    string name;
+    int age;
+    string city;
+    string college;
+    string profession;
+    string animal;
+    string petName;
+
+    // Get the user's name
+    cout << "Enter your name: ";
+    getline(cin, name);
+
+    // Get the user's age
+    cout << "Enter your age: ";
+    cin >> age;
+
+    // Consume the remaining newline
+    cin.ignore();
+
+    // Get the name of a city
+    cout << "Enter the name of a city: ";
+    getline(cin, city);
+
+    // Get the name of a college
+    cout << "Enter the name of a college: ";
+    getline(cin, college);
+
+    // Get a profession
+    cout << "Enter a profession: ";
+    getline(cin, profession);
+
+    // Get a type of animal
+    cout << "Enter a type of animal: ";
+    getline(cin, animal);
+
+    // Get a pet's name
+    cout << "Enter a pet's name: ";
+    getline(cin, petName);
+
+    // Display the story
+    cout << "There once was a person named " << name << " who lived in " << city << ". At the age of " << age << ", " << name << " went to college at " << college << ". " << name << " graduated and went to work as a " << profession << ". Then, " << name << " adopted a(n) " << animal << " named " << petName << ". They both lived happily ever after!" << endl;
+
+    return 0;
+}
