374d307def456009dbae1007deaa5204e7615d6a
TW Webb <twwebb1@me.com>
2022-12-09 22:54:25 -0500

    Chapter 3 challenge 18 complete.

--- LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_18.cpp
+++ LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_18.cpp
@@ -18,3 +18,32 @@ pi = 3.14159
 Make sure the output of the program displays the number of slices in fixed point
 notation, rounded to one decimal place of precision. Use a named constant for pi.
 */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    // Declare variables
+    const double PI = 3.14159;
+    double diameter, radius, area, slices;
+
+    // Get the diameter of the pizza
+    cout << "Enter the diameter of the pizza: ";
+    cin >> diameter;
+
+    // Calculate the radius of the pizza
+    radius = diameter / 2;
+
+    // Calculate the area of the pizza
+    area = PI * pow(radius, 2);
+
+    // Calculate the number of slices
+    slices = area / 14.125;
+
+    // Display the number of slices
+    cout << "The number of slices is " << slices << endl;
+
+    return 0;
+}
