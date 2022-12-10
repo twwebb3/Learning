fb85f79c89319f2e388e649a0280eac97b9892e2
TW Webb <twwebb1@me.com>
2022-12-09 22:58:31 -0500

    Chapter 3 challenge 19 complete.

--- LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_19.cpp
+++ LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_19.cpp
@@ -5,3 +5,39 @@ expected to eat an average of four slices. The program should ask the user for t
 number of people who will be at the party and for the diameter of the pizzas to be
 ordered. It should then calculate and display the number of pizzas to purchase.
 */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    // Declare variables
+    const double PI = 3.14159;
+    double diameter, radius, area, slices, people, pizzas;
+
+    // Get the number of people
+    cout << "Enter the number of people: ";
+    cin >> people;
+
+    // Get the diameter of the pizza
+    cout << "Enter the diameter of the pizza: ";
+    cin >> diameter;
+
+    // Calculate the radius of the pizza
+    radius = diameter / 2;
+
+    // Calculate the area of the pizza
+    area = PI * pow(radius, 2);
+
+    // Calculate the number of slices
+    slices = area / 14.125;
+
+    // Calculate the number of pizzas
+    pizzas = people * 4 / slices;
+
+    // Display the number of pizzas
+    cout << "The number of pizzas that need to be purchased is " << pizzas << endl;
+
+    return 0;
+}
