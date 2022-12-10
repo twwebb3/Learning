7a4d599838dcdb21071500c921decac7086d16b2
TW Webb <twwebb1@me.com>
2022-12-09 22:38:21 -0500

    Chapter 3 challenge 16 complete.

--- LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_16.cpp
+++ LearnCpp/Gaddis/Chapter3/gaddisChallengeCh3_16.cpp
@@ -13,3 +13,41 @@ Principal:			$1000.00
 Interest:			$  43.34
 Amount in Savings:  $1043.34
 */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    // Declare variables
+    double principal, rate, times, interest, amount;
+
+    // Get the principal
+    cout << "Enter the principal: ";
+    cin >> principal;
+
+    // Get the interest rate
+    cout << "Enter the interest rate: ";
+    cin >> rate;
+
+    // Get the number of times the interest is compounded
+    cout << "Enter the number of times the interest is compounded: ";
+    cin >> times;
+
+    // Calculate the interest
+    interest = principal * pow((1 + (rate / 100) / times), times) - principal;
+
+    // Calculate the amount
+    amount = principal + interest;
+
+    // Display the report
+    cout << "Interest Rate: " << rate << "%" << endl;
+    cout << "Times Compounded: " << times << endl;
+    cout << "Principal: $" << principal << endl;
+    cout << "Interest: $" << interest << endl;
+    cout << "Amount in Savings: $" << amount << endl;
+
+    return 0;
+}
+
