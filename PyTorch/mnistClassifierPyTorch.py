a6762990b0c4dae2ce570bea52fd8eb1abdba8b3
TW Webb <twwebb1@me.com>
2023-09-15 17:37:24 -0400

    Added batch normalization and dropout. Accuracy up to 98%.

--- PyTorch/mnistClassifierPyTorch.py
+++ PyTorch/mnistClassifierPyTorch.py
@@ -66,25 +66,44 @@ testloader = torch.utils.data.DataLoader(test, batch_size = batch_size,
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
-        self.conv2 = nn.Conv2d(32, 32, kernel_size=5)
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=5)
-        self.fc1 = nn.Linear(3*3*64, 256)
-        self.fc2 = nn.Linear(256, 10)
-    
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=5, padding=2)  # padding for preserving size
+        self.bn1 = nn.BatchNorm2d(32)
+
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=5, padding=2)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=5, padding=2)
+        self.bn3 = nn.BatchNorm2d(128)
+
+        self.fc1 = nn.Linear(3*3*128, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 10)
+
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(F.max_pool2d(self.conv2(x), 2))
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = F.relu(F.max_pool2d(self.conv3(x),2))
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = x.view(-1,3*3*64)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.max_pool2d(x, 2)
+
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.max_pool2d(x, 2)
+        x = F.dropout(x, p=0.3, training=self.training)
+
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.max_pool2d(x, 2)
+        x = F.dropout(x, p=0.4, training=self.training)
+
+        x = x.view(-1, 3 * 3 * 128)
         x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
+        x = F.dropout(x, p=0.5, training=self.training)
+
+        x = F.relu(self.fc2(x))
+        x = F.dropout(x, p=0.5, training=self.training)
+
+        x = self.fc3(x)
+
         return F.log_softmax(x, dim=1)
 
 
@@ -96,7 +115,7 @@ import torch.optim as optim
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-for epoch in range(2):
+for epoch in range(10):
     
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
