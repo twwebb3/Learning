3e29e4e880787606b0739f7dfa9a4943e0a19a3c
TW Webb <twwebb1@me.com>
2023-11-03 17:36:09 -0400

    Add do-while loop to repeatedly display menu until user selects return to main menu.

--- LearnCpp/Gaddis/Assignment/invmenu.cpp
+++ LearnCpp/Gaddis/Assignment/invmenu.cpp
@@ -7,50 +7,52 @@ using namespace std;
 
 int main()
 {
-    int choice;
+   int choice;
 
-    // Display the menu
-    cout << "    Serendipity Booksellers" << endl;
-    cout << "      Inventory Database" << endl;
+   do
+   {
+       // Display the menu
+       cout << "   Serendipity Booksellers" << endl;
+       cout << "     Inventory Database" << endl;
 
-    cout << "1. Look Up a Book" << endl;
-    cout << "2. Add a Book" << endl;
-    cout << "3. Edit a Book's Record" << endl;
-    cout << "4. Delete a Book" << endl;
-    cout << "5. Return to the Main Menu" << endl;
+       cout << "1. Look Up a Book" << endl;
+       cout << "2. Add a Book" << endl;
+       cout << "3. Edit a Book's Record" << endl;
+       cout << "4. Delete a Book" << endl;
+       cout << "5. Return to the Main Menu" << endl;
 
-    cout << endl; // Blank line
+       cout << endl; // Blank line
 
-    cout << "Enter your choice: ";
-    cin >> choice;
+       cout << "Enter your choice: ";
+       cin >> choice;
 
-    // validate the input
-    while (choice < 1 || choice > 5)
-    {
-        cout << "Please enter a number between 1 and 5: ";
-        cin >> choice;
-    }
+       // validate the input
+       while (choice < 1 || choice > 5)
+       {
+           cout << "Please enter a number between 1 and 5: ";
+           cin >> choice;
+       }
 
-    // use a switch statement to display the users choice
-    switch (choice)
-    {
-        case 1:
-            cout << "You chose 1. Look Up a Book" << endl;
-            break;
-        case 2:
-            cout << "You chose 2. Add a Book" << endl;
-            break;
-        case 3:
-            cout << "You chose 3. Edit a Book's Record" << endl;
-            break;
-        case 4:
-            cout << "You chose 4. Delete a Book" << endl;
-            break;
-        case 5:
-            cout << "You chose 5. Return to the Main Menu" << endl;
-            break;
-    }
-    
+       // use a switch statement to display the users choice
+       switch (choice)
+       {
+           case 1:
+               cout << "You chose 1. Look Up a Book" << endl;
+               break;
+           case 2:
+               cout << "You chose 2. Add a Book" << endl;
+               break;
+           case 3:
+               cout << "You chose 3. Edit a Book's Record" << endl;
+               break;
+           case 4:
+               cout << "You chose 4. Delete a Book" << endl;
+               break;
+           case 5:
+               cout << "You chose 5. Return to the Main Menu" << endl;
+               break;
+       }
+   } while (choice != 5); // Repeat the loop until the user selects 5
 
-    return 0;
+   return 0;
 }
