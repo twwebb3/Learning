140fd874c611a4d8d03d0156f652daa3fd69246e
TW Webb <twwebb1@me.com>
2023-11-03 17:30:28 -0400

    Added do-while loop to repeatedly display menu until user selects exit.

--- LearnCpp/Gaddis/Assignment/main.cpp
+++ LearnCpp/Gaddis/Assignment/main.cpp
@@ -6,49 +6,50 @@ using namespace std;
 
 int main()
 {
+   int choice;
 
-    int choice;
+   do
+   {
+       // Display the menu
+       cout << " Serendipity Booksellers" << endl;
+       cout << "       Main Menu" << endl;
 
-    // Display the menu
-    cout << "  Serendipity Booksellers" << endl;
-    cout << "        Main Menu" << endl;
+       cout << "1. Cashier Module" << endl;
+       cout << "2. Inventory Database Module" << endl;
+       cout << "3. Report Module" << endl;
+       cout << "4. Exit" << endl;
 
-    cout << "1. Cashier Module" << endl;
-    cout << "2. Inventory Database Module" << endl;
-    cout << "3. Report Module" << endl;
-    cout << "4. Exit" << endl;
+       cout << "Enter your choice: ";
 
-    cout << "Enter your choice: ";
+       // Allow the user to input a number between 1 and 4
+       cin >> choice;
 
-    // Allow the user to input a number between 1 and 4
-    cin >> choice;
+       // validate the input
+       while (choice < 1 || choice > 4)
+       {
+           cout << "Please enter a number between 1 and 4: ";
+           cin >> choice;
+       }
 
-    // validate the input
-    while (choice < 1 || choice > 4)
-    {
-        cout << "Please enter a number between 1 and 4: ";
-        cin >> choice;
-    }
+       cout << endl; // Blank line
 
-    cout << endl; // Blank line
+       // use a switch statement to display the users choice
+       switch (choice)
+       {
+           case 1:
+               cout << "You chose 1. Cashier Module" << endl;
+               break;
+           case 2:
+               cout << "You chose 2. Inventory Database Module" << endl;
+               break;
+           case 3:
+               cout << "You chose 3. Report Module" << endl;
+               break;
+           case 4:
+               cout << "You chose 4. Exit" << endl;
+               break;
+       }
+   } while (choice != 4); // Repeat the loop until the user selects 4
 
-    // use a switch statement to display the users choice
-    switch (choice)
-    {
-        case 1:
-            cout << "You chose 1. Cashier Module" << endl;
-            break;
-        case 2:
-            cout << "You chose 2. Inventory Database Module" << endl;
-            break;
-        case 3:
-            cout << "You chose 3. Report Module" << endl;
-            break;
-        case 4:
-            cout << "You chose 4. Exit" << endl;
-            break;
-    }
-
-
-    return 0;
+   return 0;
 }
