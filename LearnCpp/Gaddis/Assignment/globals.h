0b8cc027e7c9a3e2c34d02dc6e75b05bfad2b921
TW Webb <twwebb1@me.com>
2023-12-15 15:53:32 -0500

    Added globals header file so they can be used across modules.

---
+++ LearnCpp/Gaddis/Assignment/globals.h
@@ -0,0 +1,13 @@
+#include <string>
+using namespace std;
+
+const int ROWS = 20;
+
+extern string bookTitle[ROWS];
+extern string isbn[ROWS];
+extern string author[ROWS];
+extern string publisher[ROWS];
+extern string dateAdded[ROWS]; // MM/DD/YYYY
+extern int qtyOnHand[ROWS];
+extern double wholesale[ROWS];
+extern double retail[ROWS];
