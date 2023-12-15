37e4c728776c70343ecaf9c82780e306955498b0
TW Webb <twwebb1@me.com>
2023-12-15 15:54:14 -0500

    Remove globals as parameters in function.

--- LearnCpp/Gaddis/Assignment/bookinfo.h
+++ LearnCpp/Gaddis/Assignment/bookinfo.h
@@ -1,3 +1,3 @@
 
 // modify function to take arrays as parameters
-int bookInfo(string isbn[], string title[], string author[], string publisher[], string date[], int qty[], double wholesale[], double retail[]);
+int bookInfo();
