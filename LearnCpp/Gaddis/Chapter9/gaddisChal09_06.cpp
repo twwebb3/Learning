3ae09b2324bbee44b9dcddcda291084334e53551
TW Webb <twwebb1@me.com>
2023-12-19 10:52:53 -0500

    Chapter 9 challenge 6 complete.

---
+++ LearnCpp/Gaddis/Chapter9/gaddisChal09_06.cpp
@@ -0,0 +1,97 @@
+/*  Case Study Modification #1
+Modify Program 9-17 (the United Cause case study program) so it can be used with 
+any set of donations. The program should dynamically allocate the donations array 
+and ask the user to input its values.
+*/
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void arrSelectSort(int *[], int);
+void showArray(const int [], int);
+void showArrPtr(int *[], int);
+
+int main()
+{
+    int numDonations;
+
+    cout << "How many donations were made? ";
+    cin >> numDonations;
+
+    int *donations = new int[numDonations];
+
+    for (int i = 0; i < numDonations; i++)
+    {
+        cout << "Enter donation #" << i + 1 << ": ";
+        cin >> donations[i];
+    }
+
+    int **arrPtr = new int*[numDonations];
+
+    for (int i = 0; i < numDonations; i++)
+    {
+        arrPtr[i] = &donations[i];
+    }
+
+    arrSelectSort(arrPtr, numDonations);
+
+    cout << "The donations, sorted in ascending order, are: \n";
+    showArrPtr(arrPtr, numDonations);
+
+    cout << "The donations, in their original order, are: \n";
+    showArray(donations, numDonations);
+
+    delete [] donations;
+    donations = nullptr;
+
+    delete [] arrPtr;
+    arrPtr = nullptr;
+
+    return 0;
+}
+
+void arrSelectSort(int *arr[], int size)
+{
+    int startScan, minIndex;
+    int *minElem;
+
+    for (startScan = 0; startScan < (size - 1); startScan++)
+    {
+        minIndex = startScan;
+        minElem = arr[startScan];
+
+        for (int index = startScan + 1; index < size; index++)
+        {
+            if (*(arr[index]) < *minElem)
+            {
+                minElem = arr[index];
+                minIndex = index;
+            }
+        }
+
+        arr[minIndex] = arr[startScan];
+        arr[startScan] = minElem;
+    }
+}
+
+void showArray(const int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+}
+
+void showArrPtr(int *arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << *(arr[i]) << " ";
+    }
+
+    cout << endl;
+}
