74bd4f83380ea86f11bee9c40c03a763694c2d57
TW Webb <twwebb1@me.com>
2023-12-19 11:04:31 -0500

    Precipitation chance and type generator added.

--- Swift/BnrSwiftProgramming/Assignments/WeatherApp/WeatherApp/WeatherUtilities.swift
+++ Swift/BnrSwiftProgramming/Assignments/WeatherApp/WeatherApp/WeatherUtilities.swift
@@ -23,15 +23,34 @@ func generateCloudCoverage() -> String {
     return isCloudy ? "cloudy" : "clear"
 }
 
+func generatePrecipitationChance(isCloudy: String) -> Double {
+    if isCloudy=="cloudy" {
+        return Double.random(in: 0...1)
+    } else {
+        return 0
+    }
+}
+
+func generatePrecipitationType(precipitationChance: Double, temperature: Double) -> String {
+    if temperature<36 {
+        return "snow"
+    } else {
+        return "rain"
+    }
+}
+
 func todaysWeather() {
     let temperature = generateRandomTemp()
     let windCondition = generateWindCondition()
     let windSpeed = generateWindSpeed(isWindy: windCondition)
     let cloudCoverage = generateCloudCoverage()
+    let precipitationChance = generatePrecipitationChance(isCloudy: cloudCoverage)
+    let precipitationType = generatePrecipitationType(precipitationChance: precipitationChance, temperature: temperature)
     
     print("Here is today's weather: ")
     print("Temperature: \(temperature)°F")
     print("Wind Condition: \(windCondition)")
     print("Wind Speed: \(windSpeed)")
     print("Cloud Coverage: \(cloudCoverage)")
+    print("Precipitation: There's a \(precipitationChance)% chance of \(precipitationType).")
 }
