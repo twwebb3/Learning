ecb149f945f49dde03deb7103a3adba9e2aeb3f0
TW Webb <twwebb1@me.com>
2024-04-10 11:11:53 -0400

    New updates for deployment

--- Django/pages/django_project/settings.py
+++ Django/pages/django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-7)uvd+%qn30!1z_#-2$$kpvekvp=tru#=s(-9aj8#5n0w5y2#v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
