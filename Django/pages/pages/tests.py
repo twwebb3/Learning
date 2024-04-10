b3c38e288f7474d973b36df7ac5f5b42513f26e9
TW Webb <twwebb1@me.com>
2024-04-10 10:21:44 -0400

    Url unit tests added.

--- Django/pages/pages/tests.py
+++ Django/pages/pages/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+# pages/tests.py
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomepageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+
+
+class AboutPageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
+
+
+
