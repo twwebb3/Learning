6678ad5dd9ae2059af9319ac83827fd2ad5f8a9b
TW Webb <twwebb1@me.com>
2024-04-10 10:24:26 -0400

    Template and content unit tests added.

--- Django/pages/pages/tests.py
+++ Django/pages/pages/tests.py
@@ -12,6 +12,14 @@ class HomepageTests(SimpleTestCase):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
 
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_template_content(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "<h1>Homepage</h1>")
+
 
 
 class AboutPageTests(SimpleTestCase):
@@ -22,6 +30,14 @@ class AboutPageTests(SimpleTestCase):
     def test_url_available_by_name(self):
         response = self.client.get(reverse("about"))
         self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("about"))
+        self.assertTemplateUsed(response, "about.html")
+
+    def test_template_content(self):
+        response = self.client.get(reverse("about"))
+        self.assertContains(response, "<h1>About page</h1>")
 
 
 
