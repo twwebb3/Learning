f690bbc7a414b278f19103b32ea3e8e7d6289609
TW Webb <twwebb1@me.com>
2024-04-15 13:52:05 -0400

    creating db model

--- Django/blog/blog/views.py
+++ Django/blog/blog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+# blog/views.py
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
+
