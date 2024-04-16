3afaf0b48c8ae9ed167bf043cb5a79ddd5263d46
TW Webb <twwebb1@me.com>
2024-04-16 16:02:04 -0400

    delete post capability added

--- Django/blog/blog/views.py
+++ Django/blog/blog/views.py
@@ -1,6 +1,7 @@
 # blog/views.py
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Post
 
@@ -26,4 +27,9 @@ class BlogUpdateView(UpdateView):
     template_name = "post_edit.html"
     fields = ["title", "body"]
 
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
 
