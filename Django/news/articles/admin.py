c915fccfc49efdacb86f724289cc3948a780c652
TW Webb <twwebb1@me.com>
2024-04-27 11:02:38 -0400

    comment display

--- Django/news/articles/admin.py
+++ Django/news/articles/admin.py
@@ -1,10 +1,17 @@
 
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
 
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
 
 
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
     list_display = [
         "title",
         "body",
@@ -13,4 +20,5 @@ class ArticleAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
 
