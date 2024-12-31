9fdb89650f2af5b8b9b9a9664bac67191c7f856a
TW Webb <twwebb1@me.com>
2024-12-30 21:50:32 -0500

    single gameplay in place

--- games/tic_tac_toe/play.py
+++ games/tic_tac_toe/play.py
@@ -1,12 +1,23 @@
 
 
 
-from game.game import Game
+from game.game import Game, ComputerPlayer
 
 game = Game()
 
-# render board with sample positions
-player_one_positions = [(0, 0), (1, 1), (2, 2)]
-player_two_positions = [(0, 2), (1, 1), (2, 0)]
-game.render_board(player_one_positions, player_two_positions)
-game.render_board()
+while True:
+    game.render_board()
+    player_one_move = input('Player 1: Enter your move (row, col): ')
+    row, col = map(int, player_one_move.split(','))
+    game.update_positions(1, (row, col))
+    if game.check_winner() != 'No winner yet':
+        print(game.check_winner())
+        break
+
+    game.render_board()
+    computer = ComputerPlayer()
+    computer.update_player_number(2)
+    computer.make_move(game)
+    if game.check_winner() != 'No winner yet':
+        print(game.check_winner())
+        break
